34f32a2b4d73c46a6edd3950fc722af4defdc86f
jinliangwei <jinliangwei1@gmail.com>
2017-10-13 01:24:13 -0400

    deleted redundant files

--- makebed.sh
+++
@@ -1,2 +0,0 @@
-/share/probe/bin/probe-makebed -p BigLearning -e $1 -i jwPSU14 \
-    -n $2
--- setup_local_fs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-device=$1
-mount_pt=$2
-
-sudo mkfs.ext4 -E lazy_itable_init=0,lazy_journal_init=0 ${device}
-sudo mkdir ${mount_pt}
-sudo mount -o defaults,noatime,nodiratime ${device} ${mount_pt}
-sudo chown -R jinlianw:BigLearning ${mount_pt}
