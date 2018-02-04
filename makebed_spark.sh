9d240abe33107f05fa965acfc4328676089a03db
jinliangwei <jinliangwei1@gmail.com>
2018-02-04 01:28:30 -0700

    update orca

--- makebed_spark.sh
+++
@@ -1,7 +0,0 @@
-setup_dir=$(dirname $(readlink -f $0))
-
-echo $setup_dir
-
-/share/probe/bin/probe-makebed -p BigLearning -e $1 -i jwSparkU16 \
-    -s ${setup_dir}/jinlianw-startup \
-    -n $2
--- orca/makebed_spark.sh
+++ orca/makebed_spark.sh
@@ -2,6 +2,8 @@ setup_dir=$(dirname $(readlink -f $0))
 
 echo $setup_dir
 
-/share/probe/bin/probe-makebed -p BigLearning -e $1 -i jwSparkU16 \
-    -s ${setup_dir}/jinlianw-startup \
-    -n $2
+/share/probe/bin/probe-makebed -p BigLearning \
+			       -e $1 \
+			       -i jwSparkU16 \
+			       -s ${setup_dir}/jinlianw-startup \
+			       -n $2
